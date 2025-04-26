7b434f779460abaf7f0e2794699149eb3df61847
Ankit Kumar <ankitkr5447@gmail.com>
2025-04-27 00:52:59 +0530

    added some code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div class='nav-container'>
+
+
+
+
+        <h2>MightyNote</h2>
+        <div className='list-container'>
+            <ul>
+                <li>Dashboard</li>
+                <li>Notes</li>
+                <li>You</li>
+                
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
