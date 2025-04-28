6f9ac82a9dc5a031abb9a6b90731e730e20dc45c
Ankit Kumar <ankitkr5447@gmail.com>
2025-04-28 15:51:10 +0530

    created nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import "../assets/ham-menu.svg";
+import "../assets/logo.png";
 
 function Navbar() {
   return (
-    <div class='nav-container'>
-
-
-
-
+    <>
+      <div class="nav-container">
         <h2>MightyNote</h2>
-        <div className='list-container'>
-            <ul>
-                <li>Dashboard</li>
-                <li>Notes</li>
-                <li>You</li>
-                
-                
-            </ul>
+        <div className="list-container">
+          <ul>
+            <li>Dashboard</li>
+            <li>Notes</li>
+            <li>You</li>
+          </ul>
         </div>
-    </div>
-  )
+
+        <div className="img-container">
+          <img
+            width="48"
+            height="48"
+            src="https://img.icons8.com/fluency/48/menu--v1.png"
+            alt="menu--v1"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
